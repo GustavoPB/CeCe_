ed70dd79e823311995d1f04afb19c44c038031fd
Gustavo <g.puig.baeza@gmail.com>
2016-03-13 20:41:52 +0100

    +Added "isSymmetric" key to choose between chemostat architectures by simulation script
    This closes #2

--- cece/plugins/chemostat/Plugin.cpp
+++ cece/plugins/chemostat/Plugin.cpp
@@ -39,111 +39,175 @@ using namespace cece::simulator;
 
 /* ************************************************************************ */
 
-class ChemostatApi : public plugin::Api
-{
+class ChemostatApi: public plugin::Api {
 
-    /**
-     * @brief Returns a list of required plugins.
-     *
-     * @return
-     */
-    DynamicArray<String> requiredPlugins() const override
-    {
-        return {"obstacle"};
-    }
+	/**
+	 * @brief Returns a list of required plugins.
+	 *
+	 * @return
+	 */
+	DynamicArray<String> requiredPlugins() const override
+	{
+		return {"obstacle"};
+	}
 
+	void loadConfig(simulator::Simulation& simulation,
+			const config::Configuration& config) override
+			{
+		constexpr auto SLOPE = units::um(5);
 
-    void loadConfig(simulator::Simulation& simulation, const config::Configuration& config) override
-    {
-        constexpr auto SLOPE = units::um(5);
+		const auto worldSizeHalf = simulation.getWorldSize() * 0.5;
 
-        const auto worldSizeHalf = simulation.getWorldSize() * 0.5;
+		const auto pipeTop = config.get<units::Length>("pipe-top");
+		const auto pipeRadius = config.get<units::Length>("pipe-radius");
+		const auto size = config.get<Vector<units::Length>>("size");
+		const auto visible = config.get("visible", false);
+		const auto isSymmetric = config.get("isSymmetric", true);
 
-        const auto pipeTop = config.get<units::Length>("pipe-top");
-        const auto pipeRadius = config.get<units::Length>("pipe-radius");
-        const auto size = config.get<Vector<units::Length>>("size");
-        const auto visible = config.get("visible", false);
+		if (isSymmetric) {
+			// Upper part
+			{
+				auto obstacle = simulation.buildObject("obstacle.Polygon");
+				auto& shapes = obstacle->getMutableShapes();
+				shapes.resize(1);
 
-        // Upper part
-        {
-            auto obstacle = simulation.buildObject("obstacle.Polygon");
-            auto& shapes = obstacle->getMutableShapes();
-            shapes.resize(1);
+				StaticArray<units::Length, 4> borders { { -worldSizeHalf.getX()
+						- units::um(1), // Left
+				worldSizeHalf.getX() + units::um(1), // Right
+				worldSizeHalf.getY() + units::um(1), // Top
+				worldSizeHalf.getY() - pipeTop       // Bottom
+				} };
 
-            StaticArray<units::Length, 4> borders{{
-                -worldSizeHalf.getX() - units::um(1), // Left
-                 worldSizeHalf.getX() + units::um(1), // Right
-                 worldSizeHalf.getY() + units::um(1), // Top
-                 worldSizeHalf.getY() - pipeTop       // Bottom
-            }};
+				DynamicArray<PositionVector> vertices;
+				// Top left
+				vertices.push_back(PositionVector { borders[0], borders[2] });
+				// Top right
+				vertices.push_back(PositionVector { borders[1], borders[2] });
+				// Bottom right
+				vertices.push_back(PositionVector { borders[1], borders[3] });
+				// Bottom left
+				vertices.push_back(PositionVector { borders[0], borders[3] });
 
-            DynamicArray<PositionVector> vertices;
-            // Top left
-            vertices.push_back(PositionVector{borders[0], borders[2]});
-            // Top right
-            vertices.push_back(PositionVector{borders[1], borders[2]});
-            // Bottom right
-            vertices.push_back(PositionVector{borders[1], borders[3]});
-            // Bottom left
-            vertices.push_back(PositionVector{borders[0], borders[3]});
+				shapes[0] = Shape::makeEdges(vertices);
 
-            shapes[0] = Shape::makeEdges(vertices);
-
-            obstacle->initShapes();
+				obstacle->initShapes();
 #ifdef CECE_ENABLE_RENDER
-            obstacle->setVisible(visible);
+				obstacle->setVisible(visible);
 #endif
-        }
+			}
 
-        // Bottom part
-        {
-            const auto sizeHalf = size * 0.5;
+			// Bottom part
+			{
+				const auto sizeHalf = size * 0.5;
 
-            auto obstacle = simulation.buildObject("obstacle.Polygon");
-            auto& shapes = obstacle->getMutableShapes();
-            shapes.resize(1);
+				auto obstacle = simulation.buildObject("obstacle.Polygon");
+				auto& shapes = obstacle->getMutableShapes();
+				shapes.resize(1);
 
-            StaticArray<units::Length, 8> borders{{
-                -worldSizeHalf.getWidth() - units::um(1),                                // Left
-                 worldSizeHalf.getWidth() + units::um(1),                                // Right
-                -sizeHalf.getWidth(),                                                    // Middle Left
-                 sizeHalf.getWidth(),                                                    // Middle Right
-                 worldSizeHalf.getHeight() - pipeTop - 2 * pipeRadius,                   // Top
-                -worldSizeHalf.getHeight() - units::um(1),                               // Bottom
-                 worldSizeHalf.getHeight() - pipeTop - 2 * pipeRadius,                   // Middle Top
-                 worldSizeHalf.getHeight() - pipeTop - 2 * pipeRadius - size.getHeight() // Middle Bottom
-            }};
+				StaticArray<units::Length, 8> borders { {
+						-worldSizeHalf.getWidth() - units::um(1),			  // Left
+						worldSizeHalf.getWidth() + units::um(1), 			  // Right
+						-sizeHalf.getWidth(),                     			  // Middle Left
+						sizeHalf.getWidth(),                     			  // Middle Right
+						worldSizeHalf.getHeight() - pipeTop - 2 * pipeRadius, // Top
+						-worldSizeHalf.getHeight() - units::um(1),     		  // Bottom
+						worldSizeHalf.getHeight() - pipeTop - 2 * pipeRadius, // Middle Top
+						worldSizeHalf.getHeight() - pipeTop - 2 * pipeRadius
+								- size.getHeight() 							  // Middle Bottom
+				} };
 
-            DynamicArray<PositionVector> vertices;
-            // Bottom left
-            vertices.push_back(PositionVector{borders[0], borders[5]});
-            // Top left
-            vertices.push_back(PositionVector{borders[0], borders[6]});
-            // Middle top left
-            vertices.push_back(PositionVector{borders[2] - SLOPE, borders[6]});
-            vertices.push_back(PositionVector{borders[2], borders[6] - SLOPE});
-            // Middle bottom left
-            vertices.push_back(PositionVector{borders[2], borders[7] + SLOPE});
-            vertices.push_back(PositionVector{borders[2] + SLOPE, borders[7]});
-            // Middle bottom right
-            vertices.push_back(PositionVector{borders[3] - SLOPE, borders[7]});
-            vertices.push_back(PositionVector{borders[3], borders[7] + SLOPE});
-            // Middle top right
-            vertices.push_back(PositionVector{borders[3], borders[6] - SLOPE});
-            vertices.push_back(PositionVector{borders[3] + SLOPE, borders[6]});
-            // Top right
-            vertices.push_back(PositionVector{borders[1], borders[6]});
-            // Bottom right
-            vertices.push_back(PositionVector{borders[1], borders[5]});
+				DynamicArray<PositionVector> vertices;
+				// Bottom left
+				vertices.push_back(PositionVector { borders[0], borders[5] });
+				// Top left
+				vertices.push_back(PositionVector { borders[0], borders[6] });
+				// Middle top left
+				vertices.push_back(PositionVector { borders[2] - SLOPE, borders[6] });
+				vertices.push_back(PositionVector { borders[2], borders[6] - SLOPE });
+				// Middle bottom left
+				vertices.push_back(PositionVector { borders[2], borders[7] + SLOPE });
+				vertices.push_back(PositionVector { borders[2] + SLOPE, borders[7] });
+				// Middle bottom right
+				vertices.push_back(PositionVector { borders[3] - SLOPE, borders[7] });
+				vertices.push_back(PositionVector { borders[3], borders[7] + SLOPE });
+				// Middle top right
+				vertices.push_back(PositionVector { borders[3], borders[6] - SLOPE });
+				vertices.push_back(PositionVector { borders[3] + SLOPE, borders[6] });
+				// Top right
+				vertices.push_back(PositionVector { borders[1], borders[6] });
+				// Bottom right
+				vertices.push_back(PositionVector { borders[1], borders[5] });
 
-            shapes[0] = Shape::makeEdges(vertices);
+				shapes[0] = Shape::makeEdges(vertices);
 
-            obstacle->initShapes();
+				obstacle->initShapes();
 #ifdef CECE_ENABLE_RENDER
-            obstacle->setVisible(visible);
+				obstacle->setVisible(visible);
 #endif
-        }
-    }
+			}
+		} else {
+			const auto sizeHalf = size * 0.5;
+
+			StaticArray<units::Length, 10> borders { { -worldSizeHalf.getWidth()
+					- units::um(1), worldSizeHalf.getWidth() + units::um(1),
+					worldSizeHalf.getHeight() + units::um(1),
+					-worldSizeHalf.getHeight() - units::um(1),
+					-sizeHalf.getWidth(), sizeHalf.getWidth(),
+					worldSizeHalf.getHeight() - pipeTop,
+					worldSizeHalf.getHeight() - pipeTop - 2 * pipeRadius
+					- size.getHeight(), worldSizeHalf.getHeight()
+					- pipeTop - 2 * pipeRadius,
+					worldSizeHalf.getHeight() - pipeTop - size.getHeight() } };
+
+			{	//Upper part
+				auto obstacle = simulation.buildObject("obstacle.Polygon");
+				auto& shapes = obstacle->getMutableShapes();
+				shapes.resize(1);
+
+				DynamicArray<PositionVector> vertices;
+
+				vertices.push_back(PositionVector { borders[0], borders[2] });
+				vertices.push_back(PositionVector { borders[1], borders[2] });
+				vertices.push_back(PositionVector { borders[1], borders[9] });
+				//edge-smoothing
+				vertices.push_back(PositionVector { borders[5] + SLOPE,borders[9] });
+				vertices.push_back(PositionVector { borders[5], borders[9] + SLOPE });
+				//edge-smoothing
+				vertices.push_back(PositionVector { borders[5], borders[6] - SLOPE });
+				vertices.push_back(PositionVector { borders[5] - SLOPE, borders[6] });
+
+				vertices.push_back(PositionVector { borders[0], borders[6] });
+
+				shapes[0] = Shape::makeEdges(vertices);
+				obstacle->initShapes();
+				obstacle->setVisible(visible);
+			}
+			{	//Bottom Part
+				auto obstacle = simulation.buildObject("obstacle.Polygon");
+				auto& shapes = obstacle->getMutableShapes();
+				shapes.resize(1);
+
+				DynamicArray<PositionVector> vertices;
+
+				vertices.push_back(PositionVector { borders[0], borders[8] });
+				//edge-smoothing
+				vertices.push_back(PositionVector { borders[4] - SLOPE, borders[8] });
+				vertices.push_back(PositionVector { borders[4], borders[8] - SLOPE });
+				//edge-smoothing
+				vertices.push_back(PositionVector { borders[4], borders[7] + SLOPE });
+				vertices.push_back(PositionVector { borders[4] + SLOPE, borders[7] });
+
+				vertices.push_back(PositionVector { borders[1], borders[7] });
+				vertices.push_back(PositionVector { borders[1], borders[3] });
+				vertices.push_back(PositionVector { borders[0], borders[3] });
+
+				shapes[0] = Shape::makeEdges(vertices);
+				obstacle->initShapes();
+				obstacle->setVisible(visible);
+			}
+		}
+
+	}
 };
 
 /* ************************************************************************ */
