d11f39d31ef3bd52d328044265a0f3f760c37408
Gustavo <g.puig.baeza@gmail.com>
2016-05-08 12:22:29 +0200

    +Se añade plugin fitness compilable pero sin funcionalidad.
    This closes #21

---
+++ cece/plugins/fitness/Module.cpp
@@ -0,0 +1,322 @@
+/* ************************************************************************ */
+/* LIA UPM (c) 2016                                                         */
+/* ************************************************************************ */
+/* Department of Artifitial Intelligence                                    */
+/* Faculty of Informatics                                                   */
+/* Polytechnic University of Madrid - Spain                                 */
+/* ************************************************************************ */
+/*                                                                          */
+/* This file is part of CeCe.                                               */
+/*                                                                          */
+/* CeCe is free software: you can redistribute it and/or modify             */
+/* it under the terms of the GNU General Public License as published by     */
+/* the Free Software Foundation, either version 3 of the License, or        */
+/* (at your option) any later version.                                      */
+/*                                                                          */
+/* CeCe is distributed in the hope that it will be useful,                  */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/* GNU General Public License for more details.                             */
+/*                                                                          */
+/* You should have received a copy of the GNU General Public License        */
+/* along with CeCe.  If not, see <http://www.gnu.org/licenses/>.            */
+/*                                                                          */
+/* ************************************************************************ */
+
+// Declaration
+#include "cece/plugins/fitness/Module.hpp"
+
+// C++
+#include <random>
+#include <algorithm>
+#include <list>
+
+// CeCe
+#include "cece/core/Log.hpp"
+#include "cece/core/constants.hpp"
+#include "cece/core/TimeMeasurement.hpp"
+#include "cece/simulator/TimeMeasurement.hpp"
+#include "cece/simulator/Simulation.hpp"
+
+/* ************************************************************************ */
+
+namespace cece {
+namespace plugin {
+namespace fitness {
+
+/* ************************************************************************ */
+
+namespace {
+
+/* ************************************************************************ */
+
+std::random_device g_rd;
+
+/* ************************************************************************ */
+
+std::default_random_engine g_gen(g_rd());
+
+/* ************************************************************************ */
+
+/*TOREMOVE
+ * RealType getRelativeReceptorProportion(
+    RealType radius,
+    unsigned int numberOfRec)
+{
+    return 1 - std::exp(- numberOfRec / (4 * core::constants::PI * radius * radius));
+}
+*/
+/* ************************************************************************ */
+
+RealType getAssociationPropensity(
+    units::Duration step,
+    RealType Ka)
+{
+	return (1 - std::exp(-Ka * step.value()));
+}
+
+
+/* ************************************************************************ */
+
+RealType getDisassociationPropensity(
+    units::Duration step,
+    RealType Kd)
+{
+    return 1 - std::exp(-Kd * step.value());
+}
+
+/* ************************************************************************ */
+
+}
+
+/* ************************************************************************ */
+
+void Module::update()
+{
+    // Store time step
+    m_step = getSimulation().getTimeStep();
+
+    auto _ = measure_time("fitness", simulator::TimeMeasurement(getSimulation()));
+
+    // Get physics world
+    auto& world = getSimulation().getWorld();
+
+    // Foreach pending bodies
+    for (const auto& p : m_toJoin)
+    {
+        b2WeldJointDef joint;
+        joint.Initialize(p.bodyA, p.bodyB, p.bodyA->GetWorldCenter());
+        JointUserData* jUserData = new JointUserData();
+        jUserData->module = this;
+        jUserData->Kd = p.dConst;
+        joint.userData = jUserData;
+        world.CreateJoint(&joint);
+    }
+
+    // flush jointArray
+    m_toJoin.clear();
+
+    // Joints to remove
+    DynamicArray<b2Joint*> toRemove;
+
+    // Foreach active joints
+    for (auto joint = world.GetJointList(); joint != nullptr; joint = joint->GetNext())
+    {
+        const JointUserData* jUserData = reinterpret_cast<const JointUserData*>(joint->GetUserData());
+        // Not our joint
+        if (jUserData == nullptr)
+        {
+        	continue;
+        }
+
+        if (jUserData->guard != '@')
+        {
+			continue;
+		}
+
+        std::bernoulli_distribution dist(
+            getDisassociationPropensity(
+                m_step,
+                jUserData->Kd
+            )
+        );
+
+        if (dist(g_gen))
+        {
+            Log::debug("Released: ", joint->GetBodyA(), ", ", joint->GetBodyB());
+            //GPuig -- Stores the bodies whose contact is released
+            String bondRef = getBondRefFromObjects(joint->GetBodyA(), joint->GetBodyB());
+            m_toRelease.push_back(JointDef{bondRef, joint->GetBodyA(), joint->GetBodyB(), 0});
+            //GPuig
+            toRemove.push_back(joint);
+            delete jUserData;
+        }
+    }
+
+    // Destroy joints
+    for (auto joint : toRemove)
+        world.DestroyJoint(joint);
+}
+
+/* ************************************************************************ */
+
+void Module::loadConfig(const config::Configuration& config)
+{
+    // Configure parent
+    module::Module::loadConfig(config);
+
+    getSimulation().getWorld().SetContactListener(this);
+
+    for (auto&& c_bond : config.getConfigurations("bond"))
+    {
+        m_bonds.push_back(Bond{
+        	c_bond.get("bond-reference"),
+        	c_bond.get("pathogen"),
+        	c_bond.get("host"),
+			c_bond.get<RealType>("max-production-amount"),
+            c_bond.get<RealType>("association-constant"),
+            c_bond.get<RealType>("disassociation-constant")
+        });
+    }
+
+    //PoC - Store object types
+    for (auto&& cfg : config.getConfigurations("production"))
+        {
+    		/*ObjectDesc desc;
+    		desc.config = cfg.toMemory();*/
+    		m_objects.push_back(ObjectDesc{
+    			cfg.get("bond-reference"),
+    			cfg.get("class"),
+    			cfg.toMemory()});
+        }
+}
+
+/* ************************************************************************ */
+
+void Module::storeConfig(config::Configuration& config) const
+{
+    module::Module::storeConfig(config);
+
+    // Foreach bonds
+    for (const auto& bond : m_bonds)
+    {
+        auto bondConfig = config.addConfiguration("bond");
+        bondConfig.set("bond-reference", bond.pathogen);
+        bondConfig.set("pathogen", bond.pathogen);
+        bondConfig.set("host", bond.host);
+        bondConfig.set("max-production-amount", bond.pathogen);
+        bondConfig.set("association-constant", bond.aConst);
+        bondConfig.set("disassociation-constant", bond.dConst);
+    }
+}
+
+/* ************************************************************************ */
+
+void Module::BeginContact(b2Contact* contact)
+{
+    auto ba = contact->GetFixtureA()->GetBody();
+    auto bb = contact->GetFixtureB()->GetBody();
+    auto oa = static_cast<object::Object*>(ba->GetUserData());
+    auto ob = static_cast<object::Object*>(bb->GetUserData());
+
+    //TOREVIEW: Need for distinct types (!= xor doesnt work ???)
+    if (oa->is<plugin::cell::CellBase>() == ob->is<plugin::cell::CellBase>() ||
+		oa->is<plugin::parasite::ParasiteBase>() == ob->is<plugin::parasite::ParasiteBase>())
+            return;
+
+    plugin::cell::CellBase* ca = NULL;
+    plugin::parasite::ParasiteBase* cb = NULL;
+    if(oa->is<plugin::cell::CellBase>())
+    {
+    	ca = dynamic_cast<plugin::cell::CellBase*>(oa);
+ 	}
+    else
+    {
+    	cb = dynamic_cast<plugin::parasite::ParasiteBase*>(oa);
+    }
+  	if(ob->is<plugin::cell::CellBase>())
+    {
+    	ca = dynamic_cast<plugin::cell::CellBase*>(ob);
+    }
+    else
+    {
+    	cb = dynamic_cast<plugin::parasite::ParasiteBase*>(ob);
+    }
+
+    for (unsigned int i = 0; i < m_bonds.size(); i++)
+    {
+    	//GPuig
+    	if (isfitnessDefined(m_bonds[i], ca->getName(), cb->getName()))
+    	{
+    		//Nota: es necesario que el fago se introdujera siempre en la primera posicion de m_toJoin
+        std::bernoulli_distribution dist1(getAssociationPropensity(m_step, m_bonds[i].aConst));
+        if (dist1(g_gen))
+        {
+            Log::debug("Joined: ", ba, ", ", bb);
+            m_toJoin.push_back(JointDef{m_bonds[i].bondRef, ba, bb, m_bonds[i].dConst});
+            continue;
+        }
+    	}
+    }
+}
+
+/* ************************************************************************ */
+
+void Module::EndContact(b2Contact* contact)
+{
+
+	for(auto releasedjoin : m_toRelease)
+	{
+		//GPuig
+		auto& simulation = getSimulation();
+
+		auto oa = static_cast<object::Object*>(releasedjoin.bodyA->GetUserData());
+		auto ob = static_cast<object::Object*>(releasedjoin.bodyB->GetUserData());
+
+		plugin::cell::CellBase* ca = NULL;
+
+		if(oa->is<plugin::cell::CellBase>())
+		{
+			ca = dynamic_cast<plugin::cell::CellBase*>(oa);
+		}
+		else
+		{
+			ca = dynamic_cast<plugin::cell::CellBase*>(ob);
+		}
+
+		for(auto&& bond : m_bonds)
+		{
+			if (bond.bondRef == releasedjoin.bondRef)
+			{
+				for(auto&& obj : m_objects)
+				{
+					if (bond.bondRef == obj.bondRef)
+					{
+						auto phageAmount = bond.maxProdAmount;//TODO: should add a function of probability
+
+						for (unsigned int i = 0; i < phageAmount; i++)
+						{
+							auto object = simulation.buildObject(obj.objectClass);
+
+							object->configure(obj.config, simulation);
+
+							PositionVector pos;
+							pos = ca->getPosition();//ca is always the host
+
+							object->setPosition(pos);
+						}
+					}
+				}
+			}
+		}
+	}
+	m_toRelease.clear();
+	//GPuig
+}
+
+/* ************************************************************************ */
+}
+}
+}
+
+/* ************************************************************************ */
---
+++ cece/plugins/fitness/Module.hpp
@@ -0,0 +1,271 @@
+/* ************************************************************************ */
+/* LIA UPM (c) 2016                                                         */
+/* ************************************************************************ */
+/* Department of Artifitial Intelligence                                    */
+/* Faculty of Informatics                                                   */
+/* Polytechnic University of Madrid - Spain                                 */
+/* ************************************************************************ */
+/*                                                                          */
+/* This file is part of CeCe.                                               */
+/*                                                                          */
+/* CeCe is free software: you can redistribute it and/or modify             */
+/* it under the terms of the GNU General Public License as published by     */
+/* the Free Software Foundation, either version 3 of the License, or        */
+/* (at your option) any later version.                                      */
+/*                                                                          */
+/* CeCe is distributed in the hope that it will be useful,                  */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/* GNU General Public License for more details.                             */
+/*                                                                          */
+/* You should have received a copy of the GNU General Public License        */
+/* along with CeCe.  If not, see <http://www.gnu.org/licenses/>.            */
+/*                                                                          */
+/* ************************************************************************ */
+
+#pragma once
+
+/* ************************************************************************ */
+
+// CeCe config
+#include "cece/config.hpp"
+
+/* ************************************************************************ */
+
+#ifndef CECE_ENABLE_BOX2D_PHYSICS
+#error fitness requires physics engine
+#endif
+
+/* ************************************************************************ */
+
+// C++
+#include <utility>
+
+// CeCe
+#include "cece/core/DynamicArray.hpp"
+#include "cece/core/String.hpp"
+#include "cece/core/Real.hpp"
+#include "cece/core/Tuple.hpp"
+#include "cece/module/Module.hpp"
+#include "cece/config/Configuration.hpp"
+
+// Plugins
+#include "cece/plugins/cell/CellBase.hpp"
+#include "cece/plugins/parasite/ParasiteBase.hpp"
+#include "cece/plugins/object-generator/Module.hpp"
+
+// Physics
+#include <Box2D/Box2D.h>
+
+/* ************************************************************************ */
+
+namespace cece {
+namespace plugin {
+namespace fitness {
+
+/* ************************************************************************ */
+
+
+
+/**
+ * @brief Module for fitness.
+ */
+class Module : public module::Module, public b2ContactListener
+{
+
+// Private Structures
+private:
+
+	/**
+	* @brief Structure for storing bonds.
+	*/
+	struct Bond
+	{
+		String bondRef;
+		String pathogen;
+		String host;
+		RealType maxProdAmount;
+		RealType aConst;
+		RealType dConst;
+	};
+
+
+    /**
+	 * @brief Structure for storing created object parameters.
+	 */
+	struct ObjectDesc
+	{
+		/// Object configuration
+		String bondRef;
+		String objectClass;
+		config::Configuration config;
+	};
+
+	/**
+	 * @brief User data for joint.
+	 */
+	struct JointUserData
+	{
+		const char guard = '@';
+		Module* module;
+		RealType Kd;
+	};
+
+	/**
+	 * @brief Joint definition.
+	 */
+	struct JointDef
+	{
+		String bondRef;
+		b2Body* bodyA;
+		b2Body* bodyB;
+		RealType dConst;
+	};
+
+// Public Ctors & Dtors
+public:
+
+
+    using module::Module::Module;
+
+
+// Public Operations
+public:
+
+
+    /**
+     * @brief Load module configuration.
+     *
+     * @param config Source configuration.
+     */
+    void loadConfig(const config::Configuration& config) override;
+
+
+    /**
+     * @brief Store module configuration.
+     *
+     * @param config Output configuration.
+     */
+    void storeConfig(config::Configuration& config) const override;
+
+
+    /**
+     * @brief Update module state.
+     */
+    void update() override;
+
+
+    /**
+     * @brief Called when two fixtures begin to touch.
+     *
+     * @param contact
+     */
+    void BeginContact(b2Contact* contact) override;
+
+
+    /**
+     * @brief Called when two fixtures cease to touch.
+     *
+     * @param contact
+     */
+    void EndContact(b2Contact* contact) override;
+
+    /*
+	 * @brief Gets the bond's Reference according to the bond's definition in simulation file
+	 * @note if the names of the objects does not correspond to a bond definition "NO_DEFINED tag will be returned"
+	 */
+    String getBondRefFromObjects(b2Body* bodyA, b2Body* bodyB)
+    {
+    	//auto& ca = static_cast<object::Object*>(bodyA->GetUserData())->castThrow<plugin::cell::CellBase>();
+    	//auto& cb = static_cast<object::Object*>(bodyB->GetUserData())->castThrow<plugin::cell::CellBase>();
+    	auto oa = static_cast<object::Object*>(bodyA->GetUserData());
+    	auto ob = static_cast<object::Object*>(bodyB->GetUserData());
+
+    	plugin::cell::CellBase* ca = NULL;
+    	plugin::parasite::ParasiteBase* cb = NULL;
+    	if(oa->is<plugin::cell::CellBase>())
+    	{
+    		ca = dynamic_cast<plugin::cell::CellBase*>(oa);
+    	}
+    	else
+    	{
+    		cb = dynamic_cast<plugin::parasite::ParasiteBase*>(oa);
+    	}
+   		if(ob->is<plugin::cell::CellBase>())
+   		{
+   			ca = dynamic_cast<plugin::cell::CellBase*>(ob);
+    	}
+    	else
+    	{
+    		cb = dynamic_cast<plugin::parasite::ParasiteBase*>(ob);
+    	}
+
+    	String nameA = ca->getName();
+    	String nameB = cb->getName();
+
+    	String result = "NO_DEFINED";
+
+    	for(auto&& bond : m_bonds)
+    	{
+    		if (isfitnessDefined(bond, nameA, nameB))
+    		{
+    			result = bond.bondRef;
+    			break;
+    		}
+    	}
+
+    	return result;
+    }
+
+    /*
+     * @brief Defines whether the fitnesss is possible (as it was declared in the simulation) or not
+     */
+    //Define si la infección se puede producir atendiendo a la configuración en simulación
+    bool isfitnessDefined(Bond bondDef, String nameA, String nameB)
+    {
+    	//Check whether any of the objects in the contact is a phage
+    	//then if the other corresponds to a infectable cell (host)
+    	int result = false;
+
+    	if(!nameA.empty()  && !nameB.empty())
+    	{
+    		if (nameA == bondDef.pathogen || nameB == bondDef.pathogen)
+    		{
+    			if (nameA == bondDef.host || nameB == bondDef.host)
+    			{
+    				result = true;
+    			}
+    		}
+    	}
+    	return result;
+    }
+
+
+// Private Data Members
+private:
+
+    /// List of bodies to join
+    DynamicArray<JointDef> m_toJoin;
+
+    //List of bodies to release
+    DynamicArray<JointDef> m_toRelease;
+
+    /// Used time step.
+    units::Duration m_step;
+
+    /// List of created bonds.
+    DynamicArray<Bond> m_bonds;
+
+    /// List of objects
+    DynamicArray<ObjectDesc> m_objects;
+};
+
+
+
+/* ************************************************************************ */
+
+}
+}
+}
+
+/* ************************************************************************ */
